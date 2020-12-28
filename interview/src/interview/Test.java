f4dfa27061d285330c0e72d4845fa22115e395bb
Mahendra Patidar <mahendra.jtech@gmail.com>
2020-12-28 21:51:34 +0530

    BiPredicate

--- interview/src/interview/Test.java
+++ interview/src/interview/Test.java
@@ -1,12 +1,14 @@
 package interview;
 
-import java.util.*;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
+import java.util.function.*;
 
 public class Test {
 	public static void main(String[] args) {
-		
+		/*
+		 * IntBinaryOperator u = (i,j)-> i*j; System.out.println(u.applyAsInt(4,5));
+		 */
+		BiPredicate<Integer,Integer>  bi= (i,j)-> i>j;
+		System.out.println(bi.test(4, 5));
 	}
 
 }
