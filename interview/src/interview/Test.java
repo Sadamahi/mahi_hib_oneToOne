43d57fabbf82ebfcff6f59439dc784de6c024f15
Mahendra Patidar <mahendra.jtech@gmail.com>
2020-12-28 22:13:10 +0530

    bifunction

--- interview/src/interview/Test.java
+++ interview/src/interview/Test.java
@@ -7,8 +7,33 @@ public class Test {
 		/*
 		 * IntBinaryOperator u = (i,j)-> i*j; System.out.println(u.applyAsInt(4,5));
 		 */
-		BiPredicate<Integer,Integer>  bi= (i,j)-> i>j;
-		System.out.println(bi.test(4, 5));
+		/*
+		 * BiPredicate<Integer,Integer> bi= (i,j)-> i>j; System.out.println(bi.test(4,
+		 * 5));
+		 */
+		
+		/*
+		 * Function<Integer,String> f = (i)-> i+i+"sada";
+		 * System.out.println(f.apply(4));
+		 * 
+		 * 
+		 * IntFunction f1 = (i)-> i+i; System.out.println(f1.apply(4));
+		 * 
+		 * 
+		 * UnaryOperator<Integer> u = (i)->i*i; System.out.println(u.apply(4));
+		 * 
+		 * 
+		 * UnaryOperator<String> u1 = (i)->i+i; System.out.println(u1.apply("java"));
+		 * 
+		 * 
+		 * BiFunction<Integer,Integer,Integer> bf = (i,j)-> i+j;
+		 * System.out.println(bf.apply(4, 5)); BinaryOperator<Integer> bo =(i,j)-> i+j;
+		 * System.out.println(bo.apply(40, 5));
+		 * 
+		 * IntPredicate ip = i->i>5; System.out.println(ip.test(44));
+		 */
+		
+		
 	}
 
 }
